6c5e5d5a0997b63e91de698f8288477d903aec22
vanditpatel27 <144463026+vanditpatel27@users.noreply.github.com>
2025-03-12 11:21:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
+
 const path = "./data.json";
-const date = moment().subtract(5,'d').format();
+const date = moment().subtract(5, "days").format();
 
-const data = {
-    date: date,
-};
+const data = { date: date };
 
-jsonfile.writeFile(path, data,()=>{
-    simpleGit().add([path]).commit(date,{'--date':date}).push();
+// Write to JSON file
+jsonfile.writeFile(path, data, (err) => {
+    if (err) {
+        console.error("Error writing file:", err);
+        return;
+    }
+
+    // Initialize Git and perform commit/push
+    const git = simpleGit();
+    git.add([path])
+        .commit(date, { "--date": date })
+        .push()
+        .then(() => console.log("Successfully pushed to Git"))
+        .catch((err) => console.error("Git error:", err));
 });
